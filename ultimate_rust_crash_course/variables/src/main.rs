71f582e32a43ebebecdf77aa107cd0e43dd40fb3
JonathanGzzBen <jonathangzzben@gmail.com>
2022-11-27 05:32:31 -0600

    Use cargo fmt in variables exercise

--- ultimate_rust_crash_course/variables/src/main.rs
+++ ultimate_rust_crash_course/variables/src/main.rs
@@ -1,5 +1,5 @@
-const STARTING_MISSILES : i32 = 8;
-const READY_AMOUNT : i32 = 2;
+const STARTING_MISSILES: i32 = 8;
+const READY_AMOUNT: i32 = 2;
 
 fn main() {
     let (mut missiles, ready): (i32, i32) = (STARTING_MISSILES, READY_AMOUNT);
@@ -7,4 +7,3 @@ fn main() {
     missiles = missiles - ready;
     println!("{} missiles left", missiles);
 }
-
